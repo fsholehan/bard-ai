e48adcb49b052c1ede2b6bd1943a7d12183cdcf0
Fuad Sholehan <68955196+fsholehan@users.noreply.github.com>
2024-05-16 10:28:53 +0700

    add groq

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import Bard from "bard-ai";
 import cors from "cors"; // Import paket corsimp
-import ollama from "ollama";
+import Groq from "groq-sdk";
 
 dotenv.config({ path: ".env" });
 
@@ -11,26 +10,42 @@ const app = express();
 // Gunakan cors middleware
 app.use(cors());
 
-app.get("/anime", async (req, res) => {
-  const response = await ollama.chat({
-    model: "llama2",
-    messages: [{ role: "user", content: "Why is the sky blue?" }],
-  });
-  console.log(response.message.content);
-
-  res.json({ answer: response.message.content });
+const groq = new Groq({
+  apiKey: process.env.GROQ_API_KEY,
 });
 
-app.get("/ask", async (req, res) => {
+app.get("/anime", async (req, res) => {
   const { question } = req.query;
-  const myBard = new Bard(process.env.BARD_SESSION_KEY);
-  let myChat = myBard.createChat();
+  const response = await groq.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content:
+          "Nama kamu adalah NakaAI dan kamu hanya menjawab seputar anime, dan balas yang pakai bahasa indonesia.",
+      },
+      { role: "user", content: question },
+    ],
+    model: "llama3-70b-8192",
+  });
 
+  res.json({ answer: response.choices[0].message.content });
+});
+
+// Endpoint untuk menerima pesan dan mengirim balasan
+app.post("/chat", async (req, res) => {
   try {
-    const answer = await myChat.ask(question);
-    res.json({ answer });
+    const { message } = req.body;
+    // Kirim pesan ke Groq dan dapatkan balasan
+    const chatCompletion = await groq.chat.completions.create({
+      messages: [{ role: "user", content: message }],
+      model: "mixtral-8x7b-32768", // Ganti dengan model yang Anda inginkan
+    });
+
+    // Kirim balasan ke pengguna
+    res.json({ reply: chatCompletion.choices[0].message.content });
   } catch (error) {
-    res.status(500).json({ error });
+    console.error("Error during chat completion:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
